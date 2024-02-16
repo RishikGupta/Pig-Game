1f248b6e1fb5e306aebd906cd3868b67d4aa042a
Rishik Gupta <rishik.gupta70@gmail.com>
2024-02-15 19:19:33 -0500

    first commit

---
+++ pig_game.js
@@ -0,0 +1,16 @@
+//player 1
+//player 2
+// dice 3
+// currentScore 4
+// TotalScore 5
+// HOLD 6
+// ResetScore
+// Roll
+let player1 = false;
+let player2 = false;
+let diceValue = 0;
+let currentScore = 0;
+let totalScore = 0;
+let holdButton = false;
+let ResetScore = false;
+let Roll = false;
