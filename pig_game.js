79f97389a4343fedb6c19e31bfaa64edd9b00317
Rishik Gupta <rishik.gupta70@gmail.com>
2024-02-15 19:48:55 -0500

    second commit

--- pig_game.js
+++ pig_game.js
@@ -6,11 +6,42 @@
 // HOLD 6
 // ResetScore
 // Roll
-let player1 = false;
+let player1 = true;
 let player2 = false;
 let diceValue = 0;
-let currentScore = 0;
-let totalScore = 0;
+let currentScore1 = 0;
+let currentScore2 = 0;
+let totalScore1 = 0;
+let totalScore2 = 0;
 let holdButton = false;
 let ResetScore = false;
 let Roll = false;
+let html = "";
+const onDiceRoll = () => {
+
+    let difference = 7 - 1;
+    diceValue = Math.floor(Math.random() * difference);
+    diceValue += 1;
+    return diceValue;
+}
+
+const onHold = (score) => {
+    if(player1)
+        totalScore1 += score;
+    else
+        totalScore2 += score;
+}
+
+const currentScoreShow = (score) => {
+    if(player1)
+        currentScore1 += score;
+    else
+        currentScore2 += score;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    onDiceRoll();
+});
+
+
+
